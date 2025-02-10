5a3d8da5c13260ddb3ac062a2a78cccc2772075b
Leon Helwerda <leon.helwerda@gmail.com>
2025-02-10 23:13:06 +0100

    Command to delete receipt file and database entry

--- rechu/command/__init__.py
+++ rechu/command/__init__.py
@@ -5,6 +5,7 @@ Subcommand collection package.
 from .base import Base
 from .alembic import Alembic
 from .create import Create
+from .delete import Delete
 from .new import New
 from .read import Read
 
---
+++ tests/command/delete.py
@@ -0,0 +1,63 @@
+"""
+Tests of subcommand to remove receipt YAML files and database entries.
+"""
+
+from pathlib import Path
+from sqlalchemy import select
+from rechu.command.delete import Delete
+from rechu.io.receipt import ReceiptReader
+from rechu.models.receipt import Receipt, DiscountItems, ProductItem, Discount
+from ..database import DatabaseTestCase
+
+class DeleteTest(DatabaseTestCase):
+    """
+    Test deleting YAML files and database entries for receipts.
+    """
+
+    copies = [Path("samples/receipt-1.yml"), Path("samples/receipt-2.yml")]
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        for copy in self.copies:
+            copy.unlink(missing_ok=True)
+
+    def test_run(self) -> None:
+        """
+        Test executing the command.
+        """
+
+        path = Path("samples/receipt.yml").resolve()
+
+        with self.database as session:
+            session.add(next(ReceiptReader(path).read()))
+
+        command = Delete()
+        command.files = [str(path)]
+        command.keep = True
+        command.run()
+
+        with self.database as session:
+            self.assertIsNone(session.scalars(select(Receipt)).first())
+            self.assertIsNone(session.scalars(select(ProductItem)).first())
+            self.assertIsNone(session.scalars(select(Discount)).first())
+            self.assertIsNone(session.scalars(select(DiscountItems)).first())
+
+        with self.database as session:
+            session.add(next(ReceiptReader(path).read()))
+            for copy in self.copies:
+                copy.symlink_to(path.name)
+                self.assertEqual(copy.resolve(), path)
+                session.add(next(ReceiptReader(copy).read()))
+
+        command.files = ["receipt-1.yml", "receipt-2.yml", "missing.yml"]
+        command.keep = False
+        command.run()
+
+        with self.database as session:
+            receipts = list(session.scalars(select(Receipt)))
+            self.assertEqual(len(receipts), 1)
+            self.assertEqual(receipts[0].filename, 'receipt.yml')
+
+        self.assertTrue(path.exists())
+        for copy in self.copies:
+            self.assertFalse(copy.exists())
