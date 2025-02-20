2af2a617a566bbef1017a981fdaccb34cd618302
Leon Helwerda <leon.helwerda@gmail.com>
2025-02-20 23:54:38 +0100

    Run with pytest, provide reports to SonarCloud
    - Archive the coverage results of the jobs with identifying names
    - Collect all the coverage artifacts in the finish job
    - Do not error out from analysis/validation so that SonarQube scan can
      still take place
    - Actually create pylint report file for SonarCloud
    - Include JSON schema validation issues using SonarQube generic report
    - Ensure Python is available for validate_schema.sh
    - Add schema for pyproject.toml
    - README.md: Mention what is provided to SonarQube
    - Continue on errors in workflow for SonarQube

--- scripts/validate_schema.sh
+++ scripts/validate_schema.sh
@@ -2,12 +2,21 @@
 
 # Script to validate JSON schemas and the YAML files against those schemas.
 
+PYTHON=python
+if ! command -v $PYTHON 2>&1 >/dev/null; then
+	PYTHON=python3
+	if ! command -v $PYTHON 2>&1 >/dev/null; then
+		echo "This command can only be run in a Python 3 environment." >&2
+		exit 1
+	fi
+fi
+
 realpath() {
 	if [ ! -e "$1" ]; then
 		echo "Error: Path '$1' does not exist" >&2
 		return 1
 	fi
-	python -c "import os, sys;print(os.path.realpath(sys.argv[1]))" "$1"
+	$PYTHON -c "import os, sys;print(os.path.realpath(sys.argv[1]))" "$1"
 }
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
@@ -29,20 +38,63 @@ if [ ! -f "$DATA_DIR/$DATA_PATTERN" ]; then
 	DATA_PATTERN="$DATA_PATTERN/*.yml"
 fi
 
-echo "Validating schemas in $ROOT_DIR/schema against metaschema"
-check-jsonschema --check-metaschema $ROOT_DIR/schema/*.json
+check() {
+	schema=$1
+	shift
+	files=$*
+	if [ "$schema" = "meta" ]; then
+		args="--check-metaschema"
+	else
+		args="--schemafile $ROOT_DIR/$schema"
+	fi
+	if [ -t 0 ]; then
+		args="$args --color always"
+		green=$'\033[1;32m'
+		off=$'\e[m'
+	else
+		green=""
+		off=""
+	fi
+	set +e
+	if [ "$schema" = "schema/receipt.json" ]; then
+		output=$(check-jsonschema $args $files 2>&1 | grep -v "\d.\d\d\? is not a multiple of 0.01" | sed -e "$ s/^Schema validation errors were encountered.$//")
+		if [ "$output" = "" ]; then
+			echo "${green}ok${off} -- validation done"
+			code=0
+		else
+			echo "$output"
+			code=1
+		fi
+	else
+		check-jsonschema $args $files
+		code=$?
+	fi
+	if [ $code -ne 0 ]; then
+		check-jsonschema --output-format json $args $files 2>/dev/null | $PYTHON $ROOT_DIR/scripts/format_json_schema_report.py $schema
+	else
+		echo '{}' | $PYTHON $ROOT_DIR/scripts/format_json_schema_report.py $schema
+	fi
+	set -e
+}
 
-echo "Validating settings.toml, settings.toml.example and settings.toml.test"
+echo "Validating schemas in $ROOT_DIR/schema against metaschema"
+check meta schema/*.json
+
 if [ -f "$ROOT_DIR/settings.toml" ]; then
-	check-jsonschema --schemafile $ROOT_DIR/schema/settings.json $ROOT_DIR/settings.toml
+	echo "Validating settings.toml"
+	check schema/settings.json $ROOT_DIR/settings.toml
 fi
-check-jsonschema --schemafile $ROOT_DIR/schema/settings.json --default-filetype toml $ROOT_DIR/settings.toml.{example,test}
+echo "Validating settings.toml.example and settings.toml.test"
+check schema/settings.json --default-filetype toml $ROOT_DIR/settings.toml.{example,test}
 
 echo "Validating $DATA_DIR/products.yml"
-check-jsonschema --schemafile $ROOT_DIR/schema/products.json $DATA_DIR/products.yml
+check schema/products.json $DATA_DIR/products.yml
 
 echo "Validating receipts in $DATA_DIR/$DATA_PATTERN"
 # Ignore multipleOf errors for values that have less precision than 0.01
 green=$'\033[1;32m'
 off=$'\e[m'
-check-jsonschema --color always --schemafile $ROOT_DIR/schema/receipt.json $DATA_DIR/$DATA_PATTERN | grep -v "\d.\d\d\? is not a multiple of 0.01" | sed -e "$ s/^Schema validation errors were encountered.$/${green}ok${off} -- validation done/"
+check schema/receipt.json $DATA_DIR/$DATA_PATTERN
+
+echo "Validating pyproject.toml"
+check schema/pyproject.json $ROOT_DIR/pyproject.toml
