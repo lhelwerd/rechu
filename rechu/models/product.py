aa9797c7158e50a9995a1f839f11f60d8224c129
Leon Helwerda <leon.helwerda@gmail.com>
2025-04-25 18:26:35 +0200

    Store product label, price and discount matches

--- rechu/models/product.py
+++ rechu/models/product.py
@@ -3,9 +3,12 @@ Models for product metadata.
 """
 
 from typing import Optional
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
-from .base import Base
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base import Base, Price
+
+_CASCADE_OPTIONS = "all, delete-orphan"
+_PRODUCT_REF = "product.id"
 
 class Product(Base): # pylint: disable=too-few-public-methods
     """
@@ -16,6 +19,17 @@ class Product(Base): # pylint: disable=too-few-public-methods
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     shop: Mapped[str] = mapped_column(String(32)) # shop.key
+
+    # Matchers
+    labels: Mapped[list["LabelMatch"]] = \
+        relationship(back_populates="product", cascade=_CASCADE_OPTIONS,
+                     passive_deletes=True)
+    prices: Mapped[list["PriceMatch"]] = \
+        relationship(back_populates="product", cascade=_CASCADE_OPTIONS,
+                     passive_deletes=True)
+    discounts: Mapped[list["DiscountMatch"]] = \
+        relationship(back_populates="product", cascade=_CASCADE_OPTIONS,
+                     passive_deletes=True)
 
     # Descriptors
     brand: Mapped[Optional[str]]
@@ -37,8 +51,61 @@ class Product(Base): # pylint: disable=too-few-public-methods
 
     def __repr__(self) -> str:
         return (f"Product(id={self.id!r}, shop={self.shop!r}, "
-                f"brand={self.brand!r}, description={self.description!r}, "
+                f"labels={self.labels!r}, prices={self.prices!r}, "
+                f"discounts={self.discounts!r}, brand={self.brand!r}, "
+                f"description={self.description!r}, "
                 f"category={self.category!r}, type={self.type!r}, "
                 f"portions={self.portions!r}, weight={self.weight!r}, "
                 f"volume={self.volume!r}, alcohol={self.alcohol!r}, "
                 f"sku={self.sku!r}, gtin={self.gtin!r})")
+
+class LabelMatch(Base): # pylint: disable=too-few-public-methods
+    """
+    Label model for a product matching string.
+    """
+
+    __tablename__ = "product_label_match"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey(_PRODUCT_REF,
+                                                       ondelete='CASCADE'))
+    product: Mapped[Product] = relationship(back_populates="labels")
+    name: Mapped[str]
+
+    def __repr__(self) -> str:
+        return repr(self.name)
+
+class PriceMatch(Base): # pylint: disable=too-few-public-methods
+    """
+    Price model for a product matching value, which may be part of a value range
+    or time interval.
+    """
+
+    __tablename__ = "product_price_match"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey(_PRODUCT_REF,
+                                                       ondelete='CASCADE'))
+    product: Mapped[Product] = relationship(back_populates="prices")
+    value: Mapped[Price]
+    indicator: Mapped[Optional[str]]
+
+    def __repr__(self) -> str:
+        return repr(self.value) if self.indicator is None else \
+            repr((self.indicator, self.value))
+
+class DiscountMatch(Base): # pylint: disable=too-few-public-methods
+    """
+    Discount label model for a product matching string.
+    """
+
+    __tablename__ = "product_discount_match"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey(_PRODUCT_REF,
+                                                       ondelete='CASCADE'))
+    product: Mapped[Product] = relationship(back_populates="discounts")
+    label: Mapped[str]
+
+    def __repr__(self) -> str:
+        return repr(self.label)
--- tests/models/product.py
+++ tests/models/product.py
@@ -2,7 +2,7 @@
 Tests for product metadata model.
 """
 
-from rechu.models.product import Product
+from rechu.models.product import Product, LabelMatch, PriceMatch, DiscountMatch
 from tests.database import DatabaseTestCase
 
 class ProductTest(DatabaseTestCase):
@@ -20,15 +20,24 @@ class ProductTest(DatabaseTestCase):
                           weight='750g', volume='1l', alcohol='2.0%',
                           sku='1234', gtin=1234567890123)
         self.assertEqual(repr(product),
-                         "Product(id=None, shop='id', brand='abc', "
-                         "description='def', category='foo', type='bar', "
-                         "portions=12, weight='750g', volume='1l', "
-                         "alcohol='2.0%', sku='1234', gtin=1234567890123)")
+                         "Product(id=None, shop='id', labels=[], prices=[], "
+                         "discounts=[], brand='abc', description='def', "
+                         "category='foo', type='bar', portions=12, "
+                         "weight='750g', volume='1l', alcohol='2.0%', "
+                         "sku='1234', gtin=1234567890123)")
+        product.labels = [LabelMatch(product=product, name='label')]
+        product.prices = [
+            PriceMatch(product=product, value=1.23, indicator='minimum'),
+            PriceMatch(product=product, value=7.89, indicator='maximum')
+        ]
+        product.discounts = [DiscountMatch(product=product, label='disco')]
         with self.database as session:
             session.add(product)
             session.flush()
             self.assertEqual(repr(product),
-                             "Product(id=1, shop='id', brand='abc', "
+                             "Product(id=1, shop='id', labels=['label'], "
+                             "prices=[('minimum', 1.23), ('maximum', 7.89)], "
+                             "discounts=['disco'], brand='abc', "
                              "description='def', category='foo', type='bar', "
                              "portions=12, weight='750g', volume='1l', "
                              "alcohol='2.0%', sku='1234', gtin=1234567890123)")
